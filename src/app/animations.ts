90625836a693144f1e0729ce2ebe78ec49910e44
Salomao <salomao.machado.75@gmail.com>
2023-07-30 23:43:33 -0300

    <style> adicionando keyframe para a animação

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -124,7 +124,16 @@ export const flyInOutTrigger =
   export const shakeTrigger = trigger('shakeAnimation', [
     transition('* => *', [
       query('input.ng-invalid:focus , select.ng-invalid:focus', [
-        animate('0.5s', style({border: '4px solid red'}))
+        animate('0.5s', keyframes([
+          style({border: '2px solid red'}),
+          style({transform: 'translateX(0)'}),
+          style({transform: 'translateX(-10px)'}),
+          style({transform: 'translateX(10px)'}),
+          style({transform: 'translateX(-10px)'}),
+          style({transform: 'translateX(10px)'}),
+          style({transform: 'translateX(-10px)'}),
+          style({transform: 'translateX(0)'}),
+        ]))
       ])
     ])
   ])
