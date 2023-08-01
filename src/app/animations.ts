46415498962ca2e5f946658f9e8386dd98674e41
Salomao <salomao.machado.75@gmail.com>
2023-07-31 23:30:21 -0300

    <style> adicionando stagger

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,4 +1,4 @@
-import { animate, group, keyframes, query, state, style, transition, trigger } from '@angular/animations';
+import { animate, group, keyframes, query, stagger, state, style, transition, trigger } from '@angular/animations';
 
 export const highlightedStateTrigger = trigger('highlightedState', [
   state('default', style({
@@ -145,18 +145,20 @@ export const flyInOutTrigger =
           opacity: 0,
           transform: 'translateX(-100%)'
         }),
-        animate('500ms ease-out', keyframes([
-          style({
-            opacity: 1,
-            transform: 'translateX(15%)',
-            offset: 0.4
-          }),
-          style({
-            opacity: 1,
-            transform: 'translateX(0%)',
-            offset: 1
-          }),
-        ]))
+        stagger(200, [
+          animate('500ms ease-out', keyframes([
+            style({
+              opacity: 1,
+              transform: 'translateX(15%)',
+              offset: 0.4
+            }),
+            style({
+              opacity: 1,
+              transform: 'translateX(0%)',
+              offset: 1
+            }),
+          ]))
+        ]),
       ], {optional: true})
     ])
   ])
